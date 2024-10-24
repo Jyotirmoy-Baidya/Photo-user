37ec710a883d137e5f14743651f9f26df31e2f37
Deep <bahttacharyyadeep2002@gmail.com>
2024-10-24 10:43:52 +0530

    message changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,45 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
-<<<<<<< HEAD
 import SignIn from './pages/SignIn'
 import Signup from './pages/Signup'
-const App = () => {
-  return (
-      <Router>
-      <Routes>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<Signup />} />
-       <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-=======
 import LeftSection from './components/Home/LeftSection';
 import RightSection from './components/Home/RightSection';
 import Notifications from './pages/Notifications';
 import Messages from './pages/Messages';
+import MessageSection from './components/Home/MessageSection';
 
-const login = true;
+const login = true; // Assuming login is handled globally
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       {
-        login ?
+        login ? (
           <div className='h-screen bg-primary-bg flex flex-col'>
             <div className="h-full flex flex-grow">
               <LeftSection />
               <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/notifications' element={<Notifications />} />
-                <Route path='/messages' element={<Messages />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/notifications" element={<Notifications />} />
+{/* <MessageSection/> */}
+              <Route path="/messages" element={<Messages />} />
+
+
               </Routes>
-              <RightSection />
+               {/* <RightSection /> */}
+              <Routes>
+              </Routes>
             </div>
-          </div> :
+          </div>
+        ) : (
           <Routes>
-            <Route path='/login' element={<Login />} />
+            <Route path='/signin' element={<SignIn />} />
+            <Route path='/signup' element={<Signup />} />
           </Routes>
+        )
       }
-    </BrowserRouter>
->>>>>>> fea134ea1547f0dbd8ffd5c87b2e338f1d7969b1
+    </Router>
   )
 }
 
